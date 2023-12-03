12d547032b85fefe8f5595ebdf6aa698342bb36e
Holger <holger@steinhauer.software>
2023-12-03 22:46:05 +0100

    Quick'n'dirty tolution for day 03

---
+++ src/test/kotlin/day03/GearRatiosTest.kt
@@ -0,0 +1,24 @@
+package day03
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class GearRatiosTest {
+
+    val gearRatios = GearRatios()
+    val given = {}.javaClass.classLoader?.getResource("day03/demo-input.txt")?.readText()?.lines()!!
+
+    @Test
+    fun testPartNumberAdding() {
+        val actual = gearRatios.addUpPartNumbers(given)
+
+        assertEquals(4361, actual)
+    }
+
+    @Test
+    fun testGearRatioSum() {
+        val actual = gearRatios.calculateGearRatioSum(given)
+
+        assertEquals(467835, actual)
+    }
+}
